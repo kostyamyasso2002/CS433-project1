7ee75714c7f2acde1bfffb67c2f04a72e92818b9
Konstantin Myasnikov <miasnikov.km@phystech.edu>
2024-10-16 16:18:12 +0200

    empty files

---
+++ implementations.py
@@ -0,0 +1,17 @@
+def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
+    return None
+
+def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
+    return None
+
+def least_squares(y, tx):
+    return None
+
+def ridge_regression(y, tx, lambda_):
+    return None
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    return None
+
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+    return None
